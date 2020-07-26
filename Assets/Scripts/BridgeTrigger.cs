77a8c38073532339b8ade8ed54ab8ae2bffbe916
Yerkhan Baimuratov <yerluck@gmail.com>
2020-07-26 17:06:46 +0600

    add bridge

---
+++ Assets/Scripts/BridgeTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeTrigger : MonoBehaviour
+{
+    //TODO: add actual action
+    private void OnTriggerEnter2D(Collider2D other) {
+        GameEvents.Instance.BridgeTriggerEnter();    
+    }
+}
---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : Singleton<GameEvents>
+{
+    protected GameEvents() {}
+
+    public event Action onBridgeTriggerEnter;
+    public void BridgeTriggerEnter()
+    {
+        if (onBridgeTriggerEnter != null)
+        {
+            onBridgeTriggerEnter();
+        }
+    }
+}
