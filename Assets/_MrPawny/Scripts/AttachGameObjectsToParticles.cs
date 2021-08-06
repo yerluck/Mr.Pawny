fe7c74fee354d8c2de72c44ef7f5d2487934d75e
Silentdesu <silentdesu@gmail.com>
2021-08-06 10:39:13 +0600

    Moved all files into _MrPawny folder

---
+++ Assets/_MrPawny/Scripts/AttachGameObjectsToParticles.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class AttachGameObjectsToParticles : MonoBehaviour
+{
+    public GameObject m_Prefab;
+
+    private ParticleSystem m_ParticleSystem;
+    private List<GameObject> m_Instances = new List<GameObject>();
+    private ParticleSystem.Particle[] m_Particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ParticleSystem = GetComponent<ParticleSystem>();
+        m_Particles = new ParticleSystem.Particle[m_ParticleSystem.main.maxParticles];
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        int count = m_ParticleSystem.GetParticles(m_Particles);
+
+        while (m_Instances.Count < count)
+            m_Instances.Add(Instantiate(m_Prefab, m_ParticleSystem.transform));
+
+        bool worldSpace = (m_ParticleSystem.main.simulationSpace == ParticleSystemSimulationSpace.World);
+        for (int i = 0; i < m_Instances.Count; i++)
+        {
+            m_Instances[i].transform.localScale = new Vector3(m_Particles[i].startSize, m_Particles[i].startSize, m_Particles[i].startSize);
+            if (i < count)
+            {
+                if (worldSpace)
+                    m_Instances[i].transform.position = m_Particles[i].position;
+                else
+                    m_Instances[i].transform.localPosition = m_Particles[i].position;
+                m_Instances[i].SetActive(true);
+            }
+            else
+            {
+                m_Instances[i].SetActive(false);
+            }
+        }
+    }
+}
